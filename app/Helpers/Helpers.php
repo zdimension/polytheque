63923a6fef96efe6b1feb7419ec6e9017b8d9284
Tom <zippedfire@free.fr>
2019-11-17 01:03:38 +0100

    helpers

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 function deci($x, $n=2)
 {
     return number_format($x, $n, ",", ".");
@@ -8,4 +10,50 @@ function deci($x, $n=2)
 function money($x)
 {
     return deci($x) . " €";
+}
+
+function currentYear()
+{
+    if (date('n') >= 9)
+        return date('Y');
+    else
+        return date('Y') - 1;
+}
+
+function currentYearDisp($yr=null)
+{
+    $yr = (int)($yr ?: currentYear());
+    return $yr . "-" . ($yr + 1);
+}
+
+function currentSem()
+{
+    if (date('n') >= 9)
+        return 0;
+    else
+        return 1;
+}
+
+function currentSemXML($niv)
+{
+    return \App\Donnees::getSemestre(currentYear(), $niv, currentSem());
+}
+
+function currentSemDisp($niv)
+{
+    return currentSemXML($niv)["nom"];
+}
+
+function prettyPrintXML(SimpleXMLElement $elem)
+{
+    $dom = new DOMDocument("1.0");
+    $dom->preserveWhiteSpace = false;
+    $dom->formatOutput = true;
+    $dom->loadXML($elem->asXML());
+    return $dom->saveXML();
+}
+
+function lighten(string $str)
+{
+    return preg_replace('/[^a-z0-9]/', '', Str::slug($str));
 }
