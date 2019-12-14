4edc772aa16502b5d0d31581c9badc5f8c0f98ef
Tom <zippedfire@free.fr>
2019-12-14 16:09:44 +0100

    bug changement compte

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -17,7 +17,8 @@ class UtilisateurController extends Controller
     public function modifierProfil()
     {
         $data = $this->validate(request(), array_merge(RegisterController::validationRules(), [
-            'email' => 'required|string|email|max:80|unique:users,email,' . auth()->user()->id,
+            'email' => 'required|string|email|max:80|unique:utilisateurs,email,' . auth()->user()->id,
+            'password' => 'nullable|string|min:8|confirmed',
             'password_old' => [
                 'required',
                 function ($_, $val, $fail) {
@@ -28,10 +29,10 @@ class UtilisateurController extends Controller
             ]
         ]), RegisterController::validationMessages());
 
-        if ($data["mdp"] === null)
-            unset($data["mdp"]);
-        else
-            $data["mdp"] = password_hash($data["mdp"], PASSWORD_DEFAULT);
+        if ($data["password"] !== null)
+            $data["mdp"] = password_hash($data["password"], PASSWORD_DEFAULT);
+
+        unset($data["password"]);
 
         auth()->user()->update($data);
 
