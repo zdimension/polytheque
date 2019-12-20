855456887aca9165d7c41fd3d07f627ac1fc0371
Tom <zippedfire@free.fr>
2019-12-20 14:54:17 +0100

    affichage date articles sur l'accueil

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,38 @@
 
 @section("title", "Accueil")
 
+@push("head")
+    <style>
+        a.article {
+            display: flex;
+            flex-direction: row;
+        }
+
+        a.article > span:first-child {
+            flex-grow: 1;
+            font-weight: bold;
+            font-size: 125%;
+        }
+
+        a.article > span:last-child {
+            align-self: center;
+            font-size: 80%;
+        }
+    </style>
+@endpush
+
 @section('content')
     <h2 class="mb-3">Accueil</h2>
 
-    <h6>Pas grand chose à voir ici pour l'instant. Lisez bien les <a href="{{route("alias", ["alias" => "legal"])}}">CGU</a></h6>
+    <h6>Tutos et trucs utiles en tout genre. Lisez bien les <a href="{{route("alias", ["alias" => "legal"])}}">CGU</a></h6>
 
     <hr>
     <h4 class="mb-3">Derniers articles</h4>
 
     <ul class="list-group">
         @forelse(\App\Article::where("invisible", 0)->orderByDesc("date_creation")->get() as $art)
-            <a class="list-group-item list-group-item-action" style="font-weight: bold; font-size: 125%"
-               href="{{route("article.view", ["art" => $art])}}">{{$art->titre}}</a>
+            <a class="list-group-item list-group-item-action article"
+               href="{{route("article.view", ["art" => $art])}}"><span>{{$art->titre}}</span><span>{{$art->date_creation->format("d/m/Y")}}</span></a>
         @empty
             Aucun article pour le moment.
         @endforelse
